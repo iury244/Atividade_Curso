f9cce2e500d8323ccc66d7214fe7b3f969dffa70
iury244 <141427867+iury244@users.noreply.github.com>
2023-08-04 11:19:17 -0300

    ATIVIDADE AULA 04

---
+++ questao04.py
@@ -0,0 +1,14 @@
+soma = 0 
+contador = 0
+
+while True:
+    nota = float(input("Digite a sua nota:"))
+
+    if nota == 0:
+        break
+
+    soma += nota
+    contador += 1
+
+media = soma / contador
+print(media)
